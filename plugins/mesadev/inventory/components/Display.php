17ba055e2e09ac90ccdc4a24495598e1801dcebd
Lester Ariel Mesa <lesterm@gmail.com>
2016-11-15 10:08:29 -0500

    Created display component

---
+++ plugins/mesadev/inventory/components/Display.php
@@ -0,0 +1,21 @@
+<?php namespace Mesadev\Inventory\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Display extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Display Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+}
