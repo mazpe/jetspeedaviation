80ae8b7b2be601332cf2cc68687ab82773be0553
Lester Mesa <lesterm@gmail.com>
2016-11-15 00:40:33 +0000

    MesaDev Inventory Plugin init

---
+++ plugins/mesadev/inventory/Plugin.php
@@ -0,0 +1,98 @@
+<?php namespace Mesadev\Inventory;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Inventory Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Inventory',
+            'description' => 'No description provided yet...',
+            'author'      => 'Mesadev',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        //return []; // Remove this line to activate
+
+        return [
+            'Mesadev\Inventory\Components\Listing' => 'listing',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'mesadev.inventory.some_permission' => [
+                'tab' => 'Inventory',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'inventory' => [
+                'label'       => 'Inventory',
+                'url'         => Backend::url('mesadev/inventory/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['mesadev.inventory.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+
+}
---
+++ plugins/mesadev/inventory/components/Listing.php
@@ -0,0 +1,21 @@
+<?php namespace Mesadev\Inventory\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Listing extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Listing Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+}
